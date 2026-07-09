f0b8b15df89083e4576bc8f95e3c46d68013d475
Jan Kozaruk <20jackoza06@gmail.com>
2026-07-09 18:11:43 +0200

    Remove commented out code from main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -59,62 +59,6 @@ const resizeTracker = new ResizeTracker(container, ({ physicalWidth, physicalHei
 
 resizeTracker.update();
 
-// const shaderModule = gpu.device.createShaderModule({
-//   label: 'Chunk Shader Module',
-//   code: chunkShader,
-// });
-
-// const pipeline = gpu.device.createRenderPipeline({
-//   label: 'Chunk Render Pipeline',
-//   layout: 'auto',
-//   vertex: {
-//     module: shaderModule,
-//     entryPoint: 'vsMain',
-//     buffers: [
-//       {
-//         arrayStride: 24,
-//         attributes: [
-//           { shaderLocation: 0, offset: 0, format: 'float32x3' },
-//           { shaderLocation: 1, offset: 12, format: 'float32x3' },
-//         ],
-//       },
-//     ],
-//   },
-//   fragment: {
-//     module: shaderModule,
-//     entryPoint: 'fsMain',
-//     targets: [{ format: gpu.preferredCanvasFormat }],
-//   },
-//   primitive: {
-//     topology: 'triangle-list',
-//     cullMode: 'back',
-//   },
-//   depthStencil: {
-//     depthCompare: 'less',
-//     depthWriteEnabled: true,
-//     format: depthTexture.format,
-//   },
-// });
-
-// const UNIFORM_BUFFER_SIZE = 64; // bytes -> viewProjectionMatrix (16 * 4 bytes [32-bit float])
-
-// const uniformBuffer = gpu.device.createBuffer({
-//   label: 'Common Uniform Buffer',
-//   size: UNIFORM_BUFFER_SIZE,
-//   usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-// });
-
-// const bindGroup = gpu.device.createBindGroup({
-//   label: 'Common Bind Group',
-//   layout: pipeline.getBindGroupLayout(0),
-//   entries: [
-//     {
-//       binding: 0,
-//       resource: { buffer: uniformBuffer },
-//     },
-//   ],
-// });
-
 const chunk = new Chunk(0, 0);
 chunk.generate();
 
@@ -129,44 +73,6 @@ const frameLoop = new FrameLoop(({ timestamp, deltaTime }) => {
   camera.update();
 
   worldRenderer.render(camera, timestamp, renderQueue);
-
-  // const viewProjectionMatrix = camera.viewProjectionMatrix;
-
-  // gpu.queue.writeBuffer(uniformBuffer, 0, viewProjectionMatrix.buffer, viewProjectionMatrix.byteOffset, 64);
-
-  // const commandEncoder = gpu.device.createCommandEncoder({
-  //   label: 'Cube Command Encoder',
-  // });
-
-  // const renderPass = commandEncoder.beginRenderPass({
-  //   label: 'Cube Render Pass',
-  //   colorAttachments: [
-  //     {
-  //       view: worldCanvasSurface.view,
-  //       clearValue: { r: 0.1, g: 0.1, b: 0.1, a: 1.0 },
-  //       loadOp: 'clear',
-  //       storeOp: 'store',
-  //     },
-  //   ],
-  //   depthStencilAttachment: {
-  //     view: depthTexture.view,
-  //     depthClearValue: 1.0,
-  //     depthLoadOp: 'clear',
-  //     depthStoreOp: 'store',
-  //   },
-  // });
-
-  // if (!chunkMesh.isEmpty) {
-  //   renderPass.setPipeline(pipeline);
-  //   renderPass.setVertexBuffer(0, chunkMesh.vertexBuffer);
-  //   renderPass.setIndexBuffer(chunkMesh.indexBuffer, chunkMesh.indexFormat);
-  //   renderPass.setBindGroup(0, bindGroup);
-  //   renderPass.drawIndexed(chunkMesh.indexCount);
-  // }
-
-  // renderPass.end();
-
-  // gpu.queue.submit([commandEncoder.finish()]);
 });
 
 frameLoop.start();
